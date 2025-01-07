fa6adfef37ab970e416fda8035ac1374c4377f1c
djre07 <dr2834@questar.org>
2025-01-07 08:59:43 -0500

    imports for animation

---
+++ app.js
@@ -0,0 +1,8 @@
+// import three.js
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+// OrbitControls allow camera movement
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+// GLTF file loader
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+// tween allow to create animation in camera position
+import TWEEN from 'https://cdn.jsdelivr.net/npm/@tween.js@18.5.0/dist/tween.esm.js';
